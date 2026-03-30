ae7856ea50cd7ad20ca1e1097cd72ce442e812d7
YASS92izer <yasser.abdoulaye@reseau.eseo.fr>
2026-03-30 09:28:55 +0200

    projet de base

--- app/main.c
+++ app/main.c
@@ -1,44 +1,89 @@
+/**
+ *******************************************************************************
+ * @file 	main.c
+ * @author 	jjo
+ * @date 	Mar 29, 2024
+ * @brief	Fichier principal de votre projet sur carte Nucléo STM32G431KB
+ *******************************************************************************
+ */
+
 #include "config.h"
 #include "stm32g4_sys.h"
+
 #include "stm32g4_systick.h"
 #include "stm32g4_gpio.h"
 #include "stm32g4_uart.h"
 #include "stm32g4_utils.h"
 
-#include "TFT_ili9341/stm32g4_ili9341.h"
-#include "TFT_ili9341/stm32g4_xpt2046.h"
 #include <stdio.h>
 
-#include "ui.h"
+#define BLINK_DELAY		100	//ms
 
-int16_t x_t, y_t;
-char msg[32];
+void write_LED(bool b)
+{
+	HAL_GPIO_WritePin(LED_GREEN_GPIO, LED_GREEN_PIN, b);
+}
 
-int main(void) {
-  HAL_Init();
-  SystemClock_Config();
+bool char_received(uart_id_t uart_id)
+{
+	if( BSP_UART_data_ready(uart_id) )	/* Si un caractère est reçu sur l'UART 2*/
+	{
+		/* On "utilise" le caractère pour vider le buffer de réception */
+		BSP_UART_get_next_byte(uart_id);
+		return true;
+	}
+	else
+		return false;
+}
 
-  ILI9341_Init();
-  XPT2046_init();
+void heartbeat(void)
+{
+	while(! char_received(UART2_ID) )
+	{
+		write_LED(true);
+		HAL_Delay(50);
+		write_LED(false);
+		HAL_Delay(1500);
+	}
+}
 
-  DrawMainMenu();
 
-  // CORRECTION ICI : Font_16x26
-  ILI9341_Puts(10, 220, "Touche l'ecran...  ", &Font_16x26, ILI9341_COLOR_GRAY, ILI9341_COLOR_WHITE);
+/**
+  * @brief  Point d'entrée de votre application
+  */
+int main(void)
+{
+	/* Cette ligne doit rester la première de votre main !
+	 * Elle permet d'initialiser toutes les couches basses des drivers (Hardware Abstraction Layer),
+	 * condition préalable indispensable à l'exécution des lignes suivantes.
+	 */
+	HAL_Init();
 
-  while (1)
-  {
-    if (XPT2046_getCoordinates(&x_t, &y_t, XPT2046_COORDINATE_SCREEN_RELATIVE)) {
+	/* Initialisation des périphériques utilisés dans votre programme */
+	BSP_GPIO_enable();
+	BSP_UART_init(UART2_ID,115200);
 
-        sprintf(msg, "OK! X:%03d Y:%03d ", x_t, y_t);
+	/* Indique que les printf sont dirigés vers l'UART2 */
+	BSP_SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
 
-        // CORRECTION ICI : Font_16x26
-        ILI9341_Puts(10, 220, msg, &Font_16x26, ILI9341_COLOR_BLACK, ILI9341_COLOR_GREEN);
+	/* Initialisation du port de la led Verte (carte Nucleo) */
+	BSP_GPIO_pin_config(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH,GPIO_NO_AF);
 
-        HAL_Delay(300);
+	/* Hello student */
+	printf("Hi <Student>, can you read me?\n");
 
-        // CORRECTION ICI : Font_16x26
-        ILI9341_Puts(10, 220, "Touche l'ecran...  ", &Font_16x26, ILI9341_COLOR_GRAY, ILI9341_COLOR_WHITE);
-    }
-  }
+	//heartbeat();
+
+	/* Tâche de fond, boucle infinie, Infinite loop,... quelque soit son nom vous n'en sortirez jamais */
+	while (1)
+	{
+
+		if( char_received(UART2_ID) )
+		{
+			write_LED(true);		/* write_LED? Faites un ctrl+clic dessus pour voir... */
+			HAL_Delay(BLINK_DELAY);	/* ... ça fonctionne aussi avec les macros, les variables. C'est votre nouveau meilleur ami */
+			write_LED(false);
+		}
+
+	}
 }
