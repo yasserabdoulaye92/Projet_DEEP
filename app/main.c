f888c063078703578fa4f84a564b0c1e89736e88
Rody7777 <herverody.beloboebanda@reseau.eseo.fr>
2026-04-01 11:41:16 +0200

    J

--- app/main.c
+++ app/main.c
@@ -1,89 +1,76 @@
-/**
- *******************************************************************************
- * @file 	main.c
- * @author 	jjo
- * @date 	Mar 29, 2024
- * @brief	Fichier principal de votre projet sur carte Nucléo STM32G431KB
- *******************************************************************************
- */
-
 #include "config.h"
-#include "stm32g4_sys.h"
-
-#include "stm32g4_systick.h"
-#include "stm32g4_gpio.h"
-#include "stm32g4_uart.h"
-#include "stm32g4_utils.h"
-
+#include "TFT_ili9341/stm32g4_ili9341.h"
+#include "TFT_ili9341/stm32g4_xpt2046.h"
 #include <stdio.h>
 
-#define BLINK_DELAY		100	//ms
+void SystemClock_Config(void);
+void DrawGrid(void);
+void DetectTouch(void);
 
-void write_LED(bool b)
-{
-	HAL_GPIO_WritePin(LED_GREEN_GPIO, LED_GREEN_PIN, b);
+// Variables globales pour le tactile
+int16_t x_t, y_t;
+
+// Paramètres de la grille
+#define ROWS 5
+#define COLS 2
+#define CELL_W 100
+#define CELL_H 50
+#define SPACING 15
+
+void DrawGrid(void) {
+    ILI9341_Fill(ILI9341_COLOR_WHITE); // Fond blanc
+    ILI9341_Puts(60, 5, "TEST 10 CASES", &Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_WHITE);
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            uint16_t x = 15 + (j * (CELL_W + SPACING));
+            uint16_t y = 35 + (i * (CELL_H + SPACING));
+            // Dessine le contour de la case
+            ILI9341_DrawRectangle(x, y, CELL_W, CELL_H, ILI9341_COLOR_BLACK);
+
+            // Affiche le numéro de la case
+            char num[3];
+            sprintf(num, "%d", (i * COLS) + j + 1);
+            ILI9341_Puts(x + 40, y + 15, num, &Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_WHITE);
+        }
+    }
 }
 
-bool char_received(uart_id_t uart_id)
-{
-	if( BSP_UART_data_ready(uart_id) )	/* Si un caractère est reçu sur l'UART 2*/
-	{
-		/* On "utilise" le caractère pour vider le buffer de réception */
-		BSP_UART_get_next_byte(uart_id);
-		return true;
-	}
-	else
-		return false;
-}
+int main(void) {
+    HAL_Init();
+    SystemClock_Config();
 
-void heartbeat(void)
-{
-	while(! char_received(UART2_ID) )
-	{
-		write_LED(true);
-		HAL_Delay(50);
-		write_LED(false);
-		HAL_Delay(1500);
-	}
-}
+    ILI9341_Init();
+    XPT2046_init();
 
+    DrawGrid();
 
-/**
-  * @brief  Point d'entrée de votre application
-  */
-int main(void)
-{
-	/* Cette ligne doit rester la première de votre main !
-	 * Elle permet d'initialiser toutes les couches basses des drivers (Hardware Abstraction Layer),
-	 * condition préalable indispensable à l'exécution des lignes suivantes.
-	 */
-	HAL_Init();
+    int16_t x_t, y_t; // Variables pour stocker le toucher
 
-	/* Initialisation des périphériques utilisés dans votre programme */
-	BSP_GPIO_enable();
-	BSP_UART_init(UART2_ID,115200);
+    while (1) {
+        // Lecture du tactile avec la fonction de ton fichier .h
+        if (XPT2046_getCoordinates(&x_t, &y_t, XPT2046_COORDINATE_SCREEN_RELATIVE)) {
 
-	/* Indique que les printf sont dirigés vers l'UART2 */
-	BSP_SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
+            // Parcourir les 10 zones pour voir laquelle est touchée
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    uint16_t x_pos = 15 + (j * (CELL_W + SPACING));
+                    uint16_t y_pos = 35 + (i * (CELL_H + SPACING));
 
-	/* Initialisation du port de la led Verte (carte Nucleo) */
-	BSP_GPIO_pin_config(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH,GPIO_NO_AF);
+                    // Vérification si le doigt est dans cette case
+                    if (x_t > x_pos && x_t < (x_pos + CELL_W) && y_t > y_pos && y_t < (y_pos + CELL_H)) {
 
-	/* Hello student */
-	printf("Hi <Student>, can you read me?\n");
+                        // Action : La case devient BLEUE quand on appuie
+                        ILI9341_DrawRectangle(x_pos, y_pos, CELL_W, CELL_H, ILI9341_COLOR_BLUE);
+                        ILI9341_Puts(10, 300, "Case Touchee !      ", &Font_11x18, ILI9341_COLOR_BLUE, ILI9341_COLOR_WHITE);
 
-	//heartbeat();
+                        HAL_Delay(200); // Petit délai pour voir la couleur
 
-	/* Tâche de fond, boucle infinie, Infinite loop,... quelque soit son nom vous n'en sortirez jamais */
-	while (1)
-	{
-
-		if( char_received(UART2_ID) )
-		{
-			write_LED(true);		/* write_LED? Faites un ctrl+clic dessus pour voir... */
-			HAL_Delay(BLINK_DELAY);	/* ... ça fonctionne aussi avec les macros, les variables. C'est votre nouveau meilleur ami */
-			write_LED(false);
-		}
-
-	}
+                        // Remettre la case en NOIR après l'appui
+                        ILI9341_DrawRectangle(x_pos, y_pos, CELL_W, CELL_H, ILI9341_COLOR_BLACK);
+                    }
+                }
+            }
+        }
+    }
 }
